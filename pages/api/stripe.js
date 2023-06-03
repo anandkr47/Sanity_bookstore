02ab95e11bf44e37ced861a8d799ebfd229202d1
Anand kumar <anandkumar.triden@gmail.com>
2023-06-03 21:41:49 +0530

    API UPDATE

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1NEtcUSI6Cd1ktn2FZ760Uxw' },
+          { shipping_rate: process.env.STRIPE_SHIPPING_RATES },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
